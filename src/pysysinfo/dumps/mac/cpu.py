b516f3daf64746bd7dddaeb64e557e9485cb56a6
Mahasvan <mahasvan@gmail.com>
2025-12-19 17:50:23 +0530

    Apple ARM version

--- src/pysysinfo/dumps/mac/cpu.py
+++ src/pysysinfo/dumps/mac/cpu.py
@@ -80,6 +80,8 @@ def fetch_cpu_info() -> CPUInfo:
         # Apple Silicon devices do not have machdep.cpu.vendor defined.
         cpu_info.vendor = "Apple"
 
+    # Apple Silicon machines do not have this field defined.
+    # todo: check if we can get this info another way.
     if "machdep.cpu.features" in data:
         sse_features = [f.upper() for f in data["machdep.cpu.features"].split(" ") if "SSE" in f.upper()]
         if sse_features:
@@ -99,6 +101,26 @@ def fetch_cpu_info() -> CPUInfo:
     except Exception as e:
         cpu_info.status = PartialStatus()
 
+    # Attempt to get ARM version
+    """
+    in macOS, ARM can be of version 8 or 9.
+    We can differentiate between v8 and v9 based on the presence of v9 capabilities,
+    such as SME and SME2. Apple has not implemented SVE2 capabilities to their CPUs, so we cannot use those. 
+    """
+    if "arm" in arch:
+        try:
+            SME_presence = subprocess.check_output(["sysctl", "hw.optional.arm.FEAT_SME"]).decode()
+            SME_presence = True if SME_presence.split(": ")[1].strip() == "1" else False
+
+            SME2_presence = subprocess.check_output(["sysctl", "hw.optional.arm.FEAT_SME2"]).decode()
+            SME2_presence = True if SME2_presence.split(": ")[1].strip() == "1" else False
+
+
+            if SME_presence or SME2_presence:
+                cpu_info.arch_version = "9"
+            else:
+                cpu_info.arch_version = "8"
+        except Exception as e:
+            cpu_info.status = PartialStatus()
+
     return cpu_info
-
-
