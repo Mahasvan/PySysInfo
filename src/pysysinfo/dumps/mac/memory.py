2c441c0ab66b8af88dad3748d70297426113470f
Mahasvan <mahasvan@gmail.com>
2025-12-19 19:15:31 +0530

    WIP - Mac RAM data

--- src/pysysinfo/dumps/mac/memory.py
+++ src/pysysinfo/dumps/mac/memory.py
@@ -4,7 +4,7 @@ from src.pysysinfo.dumps.mac.ioreg import corefoundation_to_native, IORegistryEn
     IORegistryEntryFromPath, kIOMasterPortDefault, kNilOptions
 from CoreFoundation import kCFAllocatorDefault
 from src.pysysinfo.models.memory_models import MemoryInfo
-from src.pysysinfo.models.status_models import FailedStatus
+from src.pysysinfo.models.status_models import FailedStatus, PartialStatus
 
 
 def fetch_memory_info() -> MemoryInfo:
@@ -39,16 +39,103 @@ def fetch_memory_info() -> MemoryInfo:
 
     modules = []
     part_no = []
+    dimm_types = []
+    slot_names = []
+    dimm_speeds = []
+    dimm_manuf = []
+    dimm_capacities = []
     sizes = []
     length = None
 
     print(interface)
-    print(interface.keys())
-    print(interface.values())
+    # print(interface.keys())
+    # print(interface.values())
 
-    print("----")
-    for k, v in interface.items():
-        print(k)
-        print(v)
+    for prop in interface:
+        val = interface[prop]
+
+        if not length and "part-number" not in prop:
+            # --> [MEMORY]: No length specified for this RAM module – critical! — (IOKit/Memory)"
+            pass
+
+        if type(val) == bytes and length is int:
+            if "reg" in prop.lower():
+                readable = [
+                    round(n * 0x010000 / 0x10)
+                    for n in val.replace(b"\x00", b"")
+                ]
+
+                for i in range(length):
+                    try:
+                        # Converts non-0 values from the 'reg' property
+                        # into readable integer values representing the memory capacity.
+                        sizes.append(
+                            readable[i]
+                        )
+                    except Exception as e:
+                        memory_info.status = PartialStatus()
+                        break
+
+            else:
+                try:
+                    val = [
+                        x.decode()
+                        for x in val.split(b"\x00")
+                        if type(x) == bytes and x.decode().strip()
+                    ]
+                except Exception as e:
+                    # --> [MEMORY]: Failed to decode bytes of RAM module – critical! — (IOKit/Memory)
+                    continue
+
+        if "part-number" in prop:
+            length = len(val)
+
+            for i in range(length):
+                part_no.append(val[i])
+
+        else:
+            for i in range(length):
+                key = ""
+                value = None
+
+                if "dimm-types" in prop.lower():
+                    key = "Type"
+                    value = val[i]
+                    dimm_types.append(val[i])
+
+                elif "slot-names" in prop.lower():
+                    key = "Slot"
+                    try:
+                        bank, channel = val[i].split("/")
+
+                        value = {"Bank": bank, "Channel": channel}
+                        slot_names.append(value)
+                    except Exception as e:
+                        # --> [MEMORY]: Failed to obtain location of current RAM module – ignoring! — (IOKit/Memory)"
+                        pass
+
+                elif "dimm-speeds" in prop.lower():
+                    key = "Frequency (MHz)"
+                    value = val[i]
+                    dimm_speeds.append(value)
+
+                elif "dimm-manufacturer" in prop.lower():
+                    key = "Manufacturer"
+                    value = val[i]
+                    dimm_manuf.append(value)
+
+                elif "reg" in prop.lower():
+                    # --> [MEMORY]: Obtained capacity size (in MBs) of current RAM module! — (IOKit/Memory)",
+                    key = "Capacity"
+                    value = f"{sizes[i]}MB"
+                    dimm_capacities.append(value)
+
+    print(part_no)
+    print(dimm_types)
+    print(dimm_speeds)
+    print(dimm_manuf)
+    print(dimm_capacities)
+    print(sizes)
+    print(slot_names)
 
     return memory_info
