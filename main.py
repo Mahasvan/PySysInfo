18a2cfb2889f671ed907820662d30c947632727e
Mahasvan <mahasvan@gmail.com>
2025-12-19 10:06:25 +0530

    pretty print json

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from src.pysysinfo.dumps.linux.linux_dump import LinuxHardwareManager
 
 
@@ -7,4 +9,9 @@ lhm.fetch_cpu_info()
 lhm.fetch_memory_info()
 lhm.fetch_storage_info()
 
-print(lhm.info)
+json_data = json.loads(lhm.info.model_dump_json())
+
+with open("response.json", "w") as f:
+    json.dump(json_data, f, indent=2)
+
+print(json.dumps(json_data, indent=2))
