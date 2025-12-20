6eccb283c1a30f7a29fc539e104d172a04a63e22
Mahasvan <mahasvan@gmail.com>
2025-12-20 22:06:45 +0530

    add comments for StatusModels

--- src/pysysinfo/models/status_models.py
+++ src/pysysinfo/models/status_models.py
@@ -18,3 +18,24 @@ class PartialStatus(StatusModel):
 class FailedStatus(StatusModel):
     string: str = "failed"
     messages: List[str] = []
+
+"""
+The intention of `messages` being List[str] is that PartialStatus can benefit from containing many messages.
+
+When changing the status of a component to PartialStatus(), we do the following.
+```
+cpu_info.status = PartialStatus(messages=cpu_info.status.messages))
+cpu_info.status.messages.append("My New Debug Message")
+```
+
+This is done because cpu_info.status may be an instance of SuccessStatus or PartialStatus.
+if it is FailureStatus, the discovery of that component would have probably stopped after reaching the failure state.
+
+The above code snippet can be replaced with the below snippet, and would yield the same result. 
+```
+if type(cpu_info.status) is PartialStatus:
+    cpu_info.status.messages.append("My New Debug Message")
+else:
+    cpu_info.status = PartialStatus(messages=["My New Debug Message"])]
+```
+"""
